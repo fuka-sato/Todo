4be250030435438299387888cea9bc1f877a0ec8
fuka-sato <61341002+fuka-sato@users.noreply.github.com>
2020-07-19 16:26:37 +0900

    deviseの導入とルートパスの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'
-
+  devise_for :users
+    devise_scope :user do
+      root to: "devise/sessions#new"
+    end
   resources :posts
+  resources :users
 end
