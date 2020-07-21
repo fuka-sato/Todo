d3497d9a3f4ed2f526a972fe8fa4376e05f65424
fuka-sato <61341002+fuka-sato@users.noreply.github.com>
2020-07-21 22:53:47 +0900

    user.rbにバリデーション記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
+
+  validates_confirmation_of :password  
+  attr_accessor :password_confirmation 
+  
+  validates :nickname,
+            presence: true,                     
+            uniqueness: true,                   
+            length: { maximum: 20 }            
+  
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i } 
+  
+  validates :password,
+            presence: true,                     
+            length: { minimum: 7 },            
+            format: { with: /\A[a-z0-9]+\z/i } 
 end
