f37e5c3f9ab489a6100200aca26822dbc2a15fb7
fuka-sato <61341002+fuka-sato@users.noreply.github.com>
2020-07-23 15:48:31 +0900

    snsファイルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,15 @@
 
 ActiveRecord::Schema.define(version: 20200719065840) do
 
+  create_table "sns_credentials", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "nickname",                            null: false
     t.string   "email",                  default: "", null: false
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 20200719065840) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "sns_credentials", "users"
 end
