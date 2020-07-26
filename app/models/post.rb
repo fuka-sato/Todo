0ec90c61e9ce6fac2e496caed94d5f5626745765
fuka-sato <61341002+fuka-sato@users.noreply.github.com>
2020-07-26 21:34:45 +0900

    postモデルのバリデーション記載

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :user
   acts_as_taggable
+
+  validates :title,
+            presence: true,                            
+            length: { maximum: 40 } 
+            
+  validates :body,
+            presence: true
+
+  validates :tag_list,
+            presence: true
 end
