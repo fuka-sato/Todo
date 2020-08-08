95716955c90423d4fba928e1c8c81e210837c030
fuka-sato <61341002+fuka-sato@users.noreply.github.com>
2020-08-08 23:40:33 +0900

    ユーザー編集ページのマークアップ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,15 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      redirect_to users_path
+      redirect_to users_path,notice: '編集が完了しました'
     else
+      flash.now[:notice] = '編集できませんでした'
       render :edit
     end
+  end
 
   private 
   def user_params
-    params.require(:user).permit(:nickname, :email)
+    params.require(:user).permit(:nickname, :email,:password)
   end
 end
