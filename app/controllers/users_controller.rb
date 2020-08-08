e14dc367011a018af8b80c1f9f16cb063f7fec82
fuka-sato <61341002+fuka-sato@users.noreply.github.com>
2020-08-08 23:10:10 +0900

    ログアウトボタンの場所の変更と、database.yamlをgitignoreから除外

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
     @user = current_user
     @posts = @user.posts.order("updated_at DESC").page(params[:page]).per(10)
   end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+
+  private 
+  def user_params
+    params.require(:user).permit(:nickname, :email)
+  end
 end
