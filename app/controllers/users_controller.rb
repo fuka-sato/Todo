f2b40210ce126d6a719bc04115b8e92663647aba
fuka-sato <61341002+fuka-sato@users.noreply.github.com>
2020-08-10 20:38:52 +0900

    indexページの部分テンプレートの切り出しとその他調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.order("updated_at DESC").page(params[:page]).per(10)
+  end
+
   def update
     if current_user.update(user_params)
       redirect_to users_path,notice: '編集が完了しました'
